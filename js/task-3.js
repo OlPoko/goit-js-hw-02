b1cd0fd5f2066a16b83aa0f8d48170f2ce75c5a5
OlPoko <s.pokotilo@gmail.com>
2024-11-03 17:31:14 +0200

    add task 1-2

---
+++ js/task-3.js
@@ -0,0 +1,28 @@
+
+ function getSubscriptionPrice(type) {
+    switch (getSubscriptionPrice) {
+       case "professional":
+          return 20;
+          break;
+    }
+    switch (getSubscriptionPrice) {
+       case "organization":
+          return 50;
+          break;
+    }
+         switch (getSubscriptionPrice) {
+            case "starter":
+               return 0;
+               break;
+         }
+         switch (getSubscriptionPrice) {
+            case "random":
+               return "Invalid subscription type!";
+               break;
+         }
+      switch (getSubscriptionPrice) {
+    case "premium": 
+      return "Invalid subscription type!" ;
+      break;     
+  }
+}
